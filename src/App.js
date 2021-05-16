c2e4ac1ecf7d0dbc2e390cb1e7440b08611f1de1
ChristianSB24 <christiansaldana10@gmail.com>
2021-05-15 23:04:25 -0500

    Added post detail view

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
